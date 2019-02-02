8b0b801a07c4b5974a021d5483f95661804cb9b3
jatinm2 <jatinm2@illinois.edu>
2019-02-02 14:00:46 -0600

    pd.read_csv uses encoding

--- Read_CSV.py
+++ Read_CSV.py
@@ -1,14 +1,6 @@
 import pandas as pd
-import csv
 
 filename = "CAN_Test_DATA.csv"
 
-with open(filename, encoding='mac_roman', newline='') as f:
-	csv_reader = csv.reader(f)
-	machine_data = list(csv_reader)
-
-machine_data = pd.DataFrame(machine_data)
-
-machine_data.columns = ['row', 'equipment_type', 'equipment_status', 'timestamp', 'can_id', 'can_name', 'can_unit', 'can_value']
-
-print(machine_data.head(3))
+machine_data = pd.read_csv(filename, encoding='mac_roman')
+print(machine_data.head())
