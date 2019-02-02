493e98a546b48672034c5dc088ba592aaa8338d3
jmoh3 <jmoh3@illinois.edu>
2019-02-02 13:57:17 -0600

    reads csv correctly

--- Read_CSV.py
+++ Read_CSV.py
@@ -9,6 +9,6 @@ with open(filename, encoding='mac_roman', newline='') as f:
 
 machine_data = pd.DataFrame(machine_data)
 
-# machine_data.columns = ['row_number', 'equipment_type', 'equipment_status', 'timestamp', 'can_id', 'can_name', 'can_unit', 'can_value']
+machine_data.columns = ['row', 'equipment_type', 'equipment_status', 'timestamp', 'can_id', 'can_name', 'can_unit', 'can_value']
 
-print(machine_data)
+print(machine_data.head(3))
