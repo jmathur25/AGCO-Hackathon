92bb3aada0f4676115b62c5ebde5834bd8e0e469
jmoh3 <jmoh3@illinois.edu>
2019-02-02 14:07:04 -0600

    row number fixed

--- Read_CSV.py
+++ Read_CSV.py
@@ -4,3 +4,7 @@ filename = "CAN_Test_DATA.csv"
 
 machine_data = pd.read_csv(filename, encoding='mac_roman')
 print(machine_data.head())
+
+print(machine_data.columns.values)
+machine_data = machine_data.drop(columns='Unnamed: 0')
+print(machine_data.head())
