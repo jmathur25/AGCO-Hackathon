7cec4a49ab5f62340d17f746751dfef0bd5ff210
jmoh3 <jmoh3@illinois.edu>
2019-02-02 13:50:03 -0600

    fixed so it runs with python3

--- Read_CSV.py
+++ Read_CSV.py
@@ -3,10 +3,12 @@ import csv
 
 filename = "CAN_Test_DATA.csv"
 
-with open(filename) as f:
+with open(filename, encoding='mac_roman', newline='') as f:
 	csv_reader = csv.reader(f)
 	machine_data = list(csv_reader)
 
 machine_data = pd.DataFrame(machine_data)
 
-print(machine_data.columns.tolist())
+# machine_data.columns = ['row_number', 'equipment_type', 'equipment_status', 'timestamp', 'can_id', 'can_name', 'can_unit', 'can_value']
+
+print(machine_data)
