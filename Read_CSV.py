b739ed053db4357688fc9c41cf46b080a570d4eb
jatinm2 <jatinm2@illinois.edu>
2019-02-02 14:30:33 -0600

    added process_id, has a bug

--- Read_CSV.py
+++ Read_CSV.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import numpy as np
 
 filename = "CAN_Test_DATA.csv"
 
 machine_data = pd.read_csv(filename, encoding='mac_roman')
+
+machine_data = machine_data.drop(columns='Unnamed: 0')
 print(machine_data.head())
 
-print(machine_data.columns.values)
-machine_data = machine_data.drop(columns='Unnamed: 0')
+process_id_count = 0
+process_day_to_id = {}
+process_id_array = []
+for i in range(len(machine_data["timestamp"])):
+    timestamp = machine_data["timestamp"][i]
+    date = timestamp[:10]
+    if date in process_day_to_id:
+        process_id = process_day_to_id[date]
+    else:
+        process_id = process_id_count
+        process_day_to_id[date] = process_id
+        process_id_count += 1
+
+    process_id_array.append(process_id)
+
+machine_data["process_id"] = np.array(process_id)
 print(machine_data.head())
