381c9a5e76459dddbaf88734d27a3d19a1345ff9
jmoh3 <jmoh3@illinois.edu>
2019-02-02 14:56:29 -0600

    fixed process id bug

--- Read_CSV.py
+++ Read_CSV.py
@@ -3,7 +3,7 @@ import numpy as np
 
 filename = "CAN_Test_DATA.csv"
 
-machine_data = pd.read_csv(filename, encoding='mac_roman')
+machine_data = pd.read_csv(filename, encoding='mac_roman', nrows=10000)
 
 machine_data = machine_data.drop(columns='Unnamed: 0')
 print(machine_data.head())
@@ -12,16 +12,16 @@ process_id_count = 0
 process_day_to_id = {}
 process_id_array = []
 for i in range(len(machine_data["timestamp"])):
-    timestamp = machine_data["timestamp"][i]
-    date = timestamp[:10]
-    if date in process_day_to_id:
-        process_id = process_day_to_id[date]
-    else:
-        process_id = process_id_count
-        process_day_to_id[date] = process_id
-        process_id_count += 1
+	timestamp = machine_data["timestamp"][i]
+	date = timestamp[:10]
+	if date in process_day_to_id.:
+		process_id = process_day_to_id[date]
+	else:
+		process_id = process_id_count
+		process_day_to_id[date] = process_id
+		process_id_count += 1
+	process_id_array.append(process_id)
 
-    process_id_array.append(process_id)
 
-machine_data["process_id"] = np.array(process_id)
-print(machine_data.head())
+machine_data['process_id'] = np.array(process_id_array)
+print(set(machine_data['process_id']))
