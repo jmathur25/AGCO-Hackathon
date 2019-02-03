f2d0a014474a8096f54582166661c82b846c723c
jatinm2 <jatinm2@illinois.edu>
2019-02-03 02:20:36 -0600

    played around with feature importance. Grain loss algorithm is on point.

--- RandomForest.py
+++ RandomForest.py
@@ -50,9 +50,14 @@ def replace_missing_vals(df, col_df, col_name, na_dict, drop_list):
 
 # splits the dataframe into a dataframe with just params and the target y as its own dataframe
 # also drops any columns not needed (put as list in skips)
-def split_df_y(df, y_col_name, skips=None, na_dict=None):
+def process_df(df, y_col_name, skips=None, na_dict=None):
     df = df.copy()
     y = df[y_col_name]
+    # print(y)
+    # for i in range(len(y)):
+    #     if df[y_col_name][i] is None:
+    #         print("here")
+    #         df.drop(i, axis=0, inplace=True)
     if skips is None:
         skips = y_col_name
     else:
@@ -85,7 +90,7 @@ def fit_dfs(df1, df2):
 
     return return_df_1, return_df_2
 
-rf = RandomForestRegressor(n_jobs=-1, bootstrap=True)
+rf = RandomForestRegressor(n_jobs=-1, bootstrap=True, n_estimators=40, min_samples_leaf=3, max_features=0.5)
 
 # runs everything in Read_CSV
 machine_data = run_all()
@@ -95,14 +100,14 @@ train_data, validate_data = split_train_validate(machine_data, 0.8)
 # creates the split dataframes for the train and validate datasets
 # the target_y_name is the name of the column we want to predict
 # the skip_fields is other columns we want to drop
-target_y_name = "BROKEN_GRAIN_ACTUAL"
-skip_fields = None
+target_y_name = "GRAIN_LOSS_Rotor"
+skip_fields = ["YIELD", "YIELD_AVERAGE", "GRAIN_LOSS_Shoe"]
 # na_dict keeps track of the columns which had nulls and their medians
 # need to use this in validate_data to make sure medians used across both dataframes
-train_data_params, train_yield, na_dict = split_df_y(train_data, target_y_name, skips=skip_fields)
+train_data_params, train_yield, na_dict = process_df(train_data, target_y_name, skips=skip_fields)
 
 # the extra na_dict param makes sure na_dict medians from train used in validate!
-validate_data_params, validate_yield, _ = split_df_y(validate_data, target_y_name, skips=skip_fields, na_dict=na_dict)
+validate_data_params, validate_yield, _ = process_df(validate_data, target_y_name, skips=skip_fields, na_dict=na_dict)
 
 # fits the columns of the dfs to each other so they should share exactly the same columns
 train_data_params, validate_data_params = fit_dfs(train_data_params, validate_data_params)
@@ -120,34 +125,33 @@ train_data_params, validate_data_params = fit_dfs(train_data_params, validate_da
 # print(validate_yield.head())
 
 # the model is fit on the train data
-rf.fit(train_data_params, train_yield)
+# log_y = np.log(train_yield.values)
+# for i in range(len(log_y)):
+#     if log_y[i] == np.inf:
+#         print('here')
+
+rf.fit(train_data_params, train_yield.values)
 # the model is evaluated using its weights on the validate)
-print("Model score using defaults")
-print(rf.score(validate_data_params, validate_yield))
-# print(np.stack(rf.predict(validate_data_params), validate_yield.values))
-
-
-# to use some more features of Random Forest
-# rf = RandomForestRegressor(n_estimators=40, min_samples_leaf=3, max_features=0.5, n_jobs=-1)
-# rf.fit(train_data_params, train_yield)
-# print("Model score using custom input parameters")
-# print(rf.score(validate_data_params, validate_yield))
+print("Model score")
+print(rf.score(validate_data_params, validate_yield.values))
+# print(rf.predict(validate_data_params))
+# print(validate_yield.values)
 
 # to see how estimators do at a tree level, search
 # preds = np.stack([t.predict(X_valid) for t in rf.estimators_])
 # on the RF doc and work from there. X_valid will have to be replaced with actual validation data variable name
 
 # this makes a table to see how important various features of rf are!!
-# def rf_feature_importance(rf, df):
-#     return pd.DataFrame({'cols':df.columns, 'importance':rf.feature_importances_}).sort_values('importance', ascending=False)
+def rf_feature_importance(rf, df):
+    return pd.DataFrame({'cols':df.columns, 'importance':rf.feature_importances_}).sort_values('importance', ascending=False)
 
-# features = rf_feature_importance(rf, train_data_params)
+features = rf_feature_importance(rf, train_data_params)
 # to see the 10 most important features
-# print(features[:10])
+print(features[:30])
 #
-# def plot_features(features):
-#     return features.plot('cols', 'importance', 'barh', figsize=(12,7), legend=False)
-# plot_features(features)
+def plot_features(features):
+    return features.plot('cols', 'importance', 'barh', figsize=(12,7), legend=False)
+plot_features(features)
 
 # add this to make sure importance meets some threshold
 # THRESHOLD = 0.005
