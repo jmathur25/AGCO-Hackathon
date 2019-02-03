e91287c12593669abe4d422a345e450872155f02
jatinm2 <jatinm2@illinois.edu>
2019-02-02 18:44:22 -0600

    worked through most RF bugs. Need to fix yield so it always has a usable value. MORE TESTING NEEDED, WORKING COMMIT.

--- RandomForest.py
+++ RandomForest.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
+from Read_CSV import run_all
 
 """
 RANDOM FOREST IMPLEMENTATION
@@ -27,42 +28,66 @@ def split_train_validate(df, frac):
 
 # replaces the missing vals in the dataframe with the median
 # also creates a new column with T/F values on whether that value is null, so RF can use it!
-def replace_missing_vals(df, col, name, na_dict):
+def replace_missing_vals(df, col_df, col_name, na_dict, drop_list):
     # checks if there are nulls in the column or if the
-    if pd.isnull(col).sum() or (name in na_dict):
+    if pd.isnull(col_df).sum() or (col_name in na_dict):
         # creates a column with T/F values per row on whether that row is null
-        df[name + '_na'] = pd.isnull(col)
+        df[col_name + '_na'] = pd.isnull(col_df)
         # na_dict keeps track of the medians found for columns
-        if name in na_dict:
-            filler = na_dict[name]
+        if col_name in na_dict:
+            filler = na_dict[col_name]
         else:
-            filler = col.median()
+            filler = col_df.median()
+
         # here is where the replacement happens
-        df[name] = col.fillna(filler)
-        na_dict[name] = filler
-    return na_dict
+        df[col_name] = col_df.fillna(filler)
+        na_dict[col_name] = filler
+        # thank you gods of pandas
+        if pd.isnull(col_df).sum() == len(col_df):
+            drop_list.append(col_name)
+    return na_dict, drop_list
 
 # splits the dataframe into a dataframe with just params and the target y as its own dataframe
 # also drops any columns not needed (put as list in skips)
 def split_df_y(df, y_col_name, skips=None, na_dict=None):
     df = df.copy()
-    y = df[y_col_name].values
+    y = df[y_col_name]
     if skips is None:
         skips = y_col_name
     else:
         skips.append(y_col_name)
     df.drop(skips, axis=1, inplace=True)
+    skips = []
     if na_dict is None:
         na_dict = {}
-    # not sure what name, column are but it's what he uses lol
-    # not sure what df.items() returns either
-    for name, column in df.items():
-        na_dict = replace_missing_vals(df, column, name, na_dict)
+    # name is column name, column is column number
+    for column_name, column_df in df.items():
+        na_dict, _ = replace_missing_vals(df, column_df, column_name, na_dict, skips)
+    df.drop(skips, axis=1, inplace=True)
     return df, y, na_dict
 
 
+def fit_dfs(df1, df2):
+    df1_cols = set(df1.columns.values)
+    df2_cols = set(df2.columns.values)
+
+    return_df_1 = df1.copy()
+    return_df_2 = df2.copy()
+    net_col_list = df1_cols.intersection(df2_cols)
+    for col in df1_cols:
+        # print(col)
+        if col not in net_col_list:
+            return_df_1.drop(col, axis=1, inplace=True)
+    for col in df2_cols:
+        if col not in net_col_list:
+            return_df_2.drop(col, axis=1, inplace=True)
+
+    return return_df_1, return_df_2
+
 rf = RandomForestRegressor(n_jobs = -1)
 
+# runs everything in Read_CSV
+machine_data = run_all()
 # splitting via the 80/20 rule
 train_data, validate_data = split_train_validate(machine_data, 0.8)
 
@@ -74,27 +99,32 @@ skip_fields = ["PROCESS_ID", "DATE", "YIELD_TOTAL", "YIELD_AVERAGE"]
 # na_dict keeps track of the columns which had nulls and their medians
 # need to use this in validate_data to make sure medians used across both dataframes
 train_data_params, train_yield, na_dict = split_df_y(train_data, target_y_name, skips=skip_fields)
+print(len(train_data_params.columns.values))
 
 # the extra na_dict param makes sure na_dict medians from train used in validate!
 validate_data_params, validate_yield, _ = split_df_y(validate_data, target_y_name, skips=skip_fields, na_dict=na_dict)
+print(len(validate_data_params.columns.values))
+
+# fits the columns of the dfs to each other so they should share exactly the same columns
+train_data_params, validate_data_params = fit_dfs(train_data_params, validate_data_params)
 
 print("This is the train parameter-only data")
-print(train_data_params.head())
-
+print(train_data_params)
+#
 print("This is the train output-only data")
 print(train_yield.head())
-
+#
 print("This is the validate parameter-only data")
-print(validate_data_params.head())
-
-print("This is the validate output-only data")
-print(validate_yield.head())
+print(validate_data_params)
+#
+# print("This is the validate output-only data")
+# print(validate_yield.head())
 
 # the model is fit on the train data
-# rf.fit(train_data_params, train_yield)
+rf.fit(train_data_params, train_yield)
 # the model is evaluated using its weights on the validate)
-# print("Model score using defaults")
-# rf.score(validate_data_params, validate_yield)
+print("Model score using defaults")
+print(rf.score(validate_data_params, validate_yield))
 
 
 # to use some more features of Random Forest
@@ -121,7 +151,7 @@ print(validate_yield.head())
 
 # add this to make sure importance meets some threshold
 # THRESHOLD = 0.005
-# to_keep = features[features.imp>THRESHOLD].cols
+# to_keep = features[features.importance>THRESHOLD].cols
 # len(to_keep)
 # fixes params to just be the ones you want
 # df_keep = machine_data[to_keep].copy()
